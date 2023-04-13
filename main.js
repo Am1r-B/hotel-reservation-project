45bea2374007fda716e29233324b6d48bdba74fd
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-04-13 23:08:24 +0330

    Make header color darker on top

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 let header = document.querySelector(".header");
 let hamburgerMenu = document.querySelector(".hamburger-menu");
 
+window.addEventListener("scroll", () => {
+  let windowPosition = window.scrollY > 0;
+  header.classList.toggle("active", windowPosition)
+})
+
 hamburgerMenu.addEventListener("click", () => {
   header.classList.toggle("menu-open");
 });
